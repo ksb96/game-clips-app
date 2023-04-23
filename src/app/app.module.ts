76400199cc7f1c6c5013d965de4bdc5180dc3763
kubstar <52975077+kubstar@users.noreply.github.com>
2023-04-23 17:31:30 +0530

    modal close -C

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
+// module
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { UserModule } from "./user/user.module";
+
+// component
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { AuthModalComponent } from './user/auth-modal/auth-modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
