0231bf88347b6c83e7267be340b668465a251340
kubstar <52975077+kubstar@users.noreply.github.com>
2023-05-16 22:53:25 +0530

    validation input -pswd --C

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -12,24 +12,29 @@ export class RegisterComponent {
     Validators.required,
     Validators.minLength(3)
   ])
-  email = new FormControl('',[
+  email = new FormControl('', [
     Validators.required,
     Validators.email
   ])
-  age = new FormControl('',[
+  age = new FormControl('', [
     Validators.required,
     Validators.min(16)
   ])
-  password = new FormControl('')
-  confirm_password = new FormControl('')
+  password = new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)
+  ])
+  confirm_password = new FormControl('', [
+    Validators.required
+  ])
   phoneNumber = new FormControl('')
 
   registerForm = new FormGroup({
-    name:this.name,
-    age:this.age,
-    email:this.email,
-    password:this.password,
-    confirm_password:this.confirm_password
+    name: this.name,
+    age: this.age,
+    email: this.email,
+    password: this.password,
+    confirm_password: this.confirm_password
   })
 
   // constructor() {
